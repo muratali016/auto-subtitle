dab07b5b26f4df29a1122d51841dbc31da3ab136
Murat A. Avcu <77502485+muratali016@users.noreply.github.com>
2024-03-24 03:23:34 +0300

    Update cli.py

--- auto_subtitle/cli.py
+++ auto_subtitle/cli.py
@@ -63,7 +63,7 @@ def main():
         audio = video.audio
 
         ffmpeg.concat(
-            video.filter('subtitles', srt_path, force_style="OutlineColour=&H40000000,BorderStyle=3"), audio, v=1, a=1
+            video.filter('subtitles', srt_path, force_style="OutlineColour=&H00FF00,BorderStyle=3"), audio, v=1, a=1
         ).output(out_path).run(quiet=True, overwrite_output=True)
 
         print(f"Saved subtitled video to {os.path.abspath(out_path)}.")
